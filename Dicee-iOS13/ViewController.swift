e5c0a8640b7fcff1cbb5fd4787c5dc5782356ea5
Victor Batista <victorbatista@Victors-Mac.local>
2021-02-19 13:45:00 -0800

    Start IOS

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var leftDiceImageView: UIImageView!
+    @IBOutlet weak var rightDiceImageView: UIImageView!
+    @IBOutlet weak var rollCountLabel: UILabel!
+    @IBOutlet weak var equalsDicesCountLabel: UILabel!
+    
+    var rollCount = 0
+    var equalsDicesCount = 0
+    
+    @IBAction func rollButton(_ sender: UIButton) {
+        rollDices()
     }
-
-
+    
+    func rollDices() {
+        let dices = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        
+        leftDiceImageView.image = dices.randomElement()
+        rightDiceImageView.image = dices.randomElement()
+        
+        rollCount += 1
+        
+        if(leftDiceImageView.image == rightDiceImageView.image){
+            equalsDicesCount += 1
+            setBorders()
+        } else {
+            clearBorders()
+        }
+        equalsDicesCountLabel.text = "Equals dices: \(equalsDicesCount)"
+        rollCountLabel.text = "Roll count: \(rollCount)"
+    }
+    
+    func clearBorders(){
+        leftDiceImageView.layer.borderWidth = 0
+        rightDiceImageView.layer.borderWidth = 0
+    }
+    
+    func setBorders(){
+        leftDiceImageView.layer.masksToBounds = true
+        leftDiceImageView.layer.borderWidth = 5
+        leftDiceImageView.layer.cornerRadius = 5
+        leftDiceImageView.layer.borderColor = #colorLiteral(red: 1, green: 0.7389414613, blue: 0.07644330912, alpha: 1)
+        rightDiceImageView.layer.masksToBounds = true
+        rightDiceImageView.layer.borderWidth = 5
+        rightDiceImageView.layer.cornerRadius = 5
+        rightDiceImageView.layer.borderColor = #colorLiteral(red: 1, green: 0.7389414613, blue: 0.07644330912, alpha: 1)
+    }
 }
 
